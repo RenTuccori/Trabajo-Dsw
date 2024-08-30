bac9b14f4fc804b40f903297eccb9d3ff66d90e8
RenTuccori <tuccorirenzo@gmail.com>
2024-08-30 15:48:44 -0300

    FUNCIONA
    JEEE

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -5,7 +5,7 @@ config.config();
 
 const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASS;
+const dbPass = process.env.DB_PASSWORD;
 const dbDatabase = process.env.DB_DATABASE;
 
 export const pool = createPool({
