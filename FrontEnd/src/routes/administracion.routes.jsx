e525631220a381b432e28e8d974988d110d7ef8e
Octavio <oberlanda@frro.utn.edu.ar>
2024-12-06 19:58:45 -0300

    ..

--- FrontEnd/src/routes/administracion.routes.jsx
+++ FrontEnd/src/routes/administracion.routes.jsx
@@ -1,31 +1,35 @@
-import { Routes, Route } from "react-router-dom";
-import AdministracionProvider from "../context/administracion/AdministracionProvider.jsx";
-import HomeAdmin from "../pages/administration/homeAdministracion.jsx";
-import { CrearSede } from "../pages/administration/crearSede.jsx";
-import { CrearEspecialidad } from "../pages/administration/crearEspecialidad.jsx";
-import { CrearObraSocial } from "../pages/administration/crearObraSocial.jsx";
-import { AsignarCombinacion } from "../pages/administration/crearCombinacion.jsx";
-import { CrearDoctor } from "../pages/administration/crearDoctor.jsx";
-import { ActualizarDoctor } from "../pages/administration/actualizarDoctor.jsx";
-import { CrearHorarios } from "../pages/administration/crearHorarios.jsx";
-
-
-
+import { Routes, Route } from 'react-router-dom';
+import AdministracionProvider from '../context/administracion/AdministracionProvider.jsx';
+import HomeAdmin from '../pages/administration/homeAdministracion.jsx';
+import { CrearSede } from '../pages/administration/crearSede.jsx';
+import { CrearEspecialidad } from '../pages/administration/crearEspecialidad.jsx';
+import { CrearObraSocial } from '../pages/administration/crearObraSocial.jsx';
+import { AsignarCombinacion } from '../pages/administration/crearCombinacion.jsx';
+import { CrearDoctor } from '../pages/administration/crearDoctor.jsx';
+import { ActualizarDoctor } from '../pages/administration/actualizarDoctor.jsx';
+import { CrearHorarios } from '../pages/administration/crearHorarios.jsx';
+import { Validacion } from './validacion.jsx';
 
 export function AdministracionRoutes() {
+  const rol = AdministracionProvider();
+
   return (
     <AdministracionProvider>
-      <Routes>
-        <Route path="/" element={<HomeAdmin />} />
-        <Route path="/crearSede" element={<CrearSede />} />
-        <Route path="/crearEsp" element={<CrearEspecialidad />} />
-        <Route path="/crearOS" element={<CrearObraSocial />} />
-        <Route path="/combinacion" element={<AsignarCombinacion />} />
-        <Route path="/horarios" element={<CrearHorarios />} />
-        <Route path="/crearDoc" element={<CrearDoctor />} />
-        <Route path="/actualizarDoc/:idDoctor" element={<ActualizarDoctor />} />
-      </Routes>
+      <Validacion rol={rol} esperado={'A'}>
+        <Routes>
+          <Route path="/" element={<HomeAdmin />} />
+          <Route path="/crearSede" element={<CrearSede />} />
+          <Route path="/crearEsp" element={<CrearEspecialidad />} />
+          <Route path="/crearOS" element={<CrearObraSocial />} />
+          <Route path="/combinacion" element={<AsignarCombinacion />} />
+          <Route path="/horarios" element={<CrearHorarios />} />
+          <Route path="/crearDoc" element={<CrearDoctor />} />
+          <Route
+            path="/actualizarDoc/:idDoctor"
+            element={<ActualizarDoctor />}
+          />
+        </Routes>
+      </Validacion>
     </AdministracionProvider>
   );
 }
-
